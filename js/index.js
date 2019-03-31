91829b9415a6d1bbc10da885be3131d5ce2b3114
Mohammed Noori <kmanoorib@gmail.com>
2019-03-31 23:21:53 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -39,14 +39,7 @@ function getCommits(el) {
 // 
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
-  const commitsList = `<ul>${commits.map(commit =>'<li><h3>' + commit.commit.author.name +
-        ' (' +
-        commit.author.login +
-        ')</h3>' +
-        commit.commit.message +
-        '</li>'
-    )
-    .join('')}</ul>`;
-  document.getElementById('details').innerHTML = commitsList;
+  const showCommits = `<ul>${commits.map(commit =>'<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`;
+  document.getElementById('details').innerHTML = showCommits;
 }
 
