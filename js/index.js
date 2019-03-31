19c871fd0ea0ea49b2f01e2caba6c73849a523d0
Mohammed Noori <kmanoorib@gmail.com>
2019-03-31 23:31:51 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -45,6 +45,6 @@ function displayCommits() {
 
 function displayBranches() {
   const branches = JSON.parse(this.responseText);
-  console.log("Hello world!")
+  return console.log("Hello world!")
   //
 }
