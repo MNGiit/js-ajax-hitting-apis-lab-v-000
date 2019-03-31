6ea43cd42c846db9af6178f9f369ddf49974cc30
Mohammed Noori <kmanoorib@gmail.com>
2019-03-31 23:11:01 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
 // your code here
+function getRepositories() {
+  // get username
+  let user = document.getElementById('username').value;
+  const req = new XMLHttpRequest();
+  // addEventListener
+  req.addEventListener('load', showRepositories);
+  // api url/users/user/repos
+  req.open('GET', 'https://api.github.com/users/' + user + '/repos');
+  req.send();
+}
+
+function showRepositories() {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        '<li>' +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
+
+
+//
+// 
+function displayCommits() {
+  
+}
+
