ac0681cf2d7ba4ac646f9a9dd6c9fa00597e57d1
Mohammed Noori <kmanoorib@gmail.com>
2019-03-31 23:58:06 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ function showRepositories() {
 ///////////////////////////////////////////////////////////////////////////////
 
 function getCommits(el) {
-  const name = el.dataset.repo;
+  const name = el.dataset.repository;
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayCommits);
   // expected: https://api.github.com/repos/octocat/Spoon-Knife/commits
